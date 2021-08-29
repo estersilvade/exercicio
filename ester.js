175876e653064c66ad4e233f7a2dcff91d6005d2
ester <estersilvade@gmail.com>
2021-08-29 12:15:41 -0300

    tentando subir arquivo

---
+++ ester.js
@@ -0,0 +1,6 @@
+var usuarios = prompt("Digite Seu Nome ")
+alert("Ola " + usuarios + " Seja bem vindo")
+var numero1= 87+96
+alert("A soma de 87+96 é igual a " + numero1)
+var numero2= 5*8
+alert("A mutiplicação de 5*8 é igual a " + numero2)
