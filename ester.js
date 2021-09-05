47740559d60afe516b59473a441937a919df3edd
ester <estersilvade@gmail.com>
2021-09-05 12:45:53 -0300

    farmacia  eu confundi

--- ester.js
+++ ester.js
@@ -1,6 +1,46 @@
-var usuarios = prompt("Digite Seu Nome ")
-alert("Ola " + usuarios + " Seja bem vindo")
-var numero1= 87+96
-alert("A soma de 87+96 é igual a " + numero1)
-var numero2= 5*8
-alert("A mutiplicação de 5*8 é igual a " + numero2)
+//let usuarios = prompt("Digite Seu Nome ")
+//alert("Ola " + usuarios + " Seja bem vindo")
+//alertlet numero1= 87+96
+//alert("A soma de 87+96 é igual a " + numero1)
+//let numero2= 5*8
+//alert("A mutiplicação de 5*8 é igual a " + numero2)
+
+
+//calcular.addEventListener("click",(e) =>{
+ // e.preventDefault();
+ // console.log("click")
+//})
+function media(){
+  let av1 = parseFloat(document.getElementById("nota1").value);
+  let av2 = parseFloat(document.getElementById("nota2").value);
+  let av3 = parseFloat(document.getElementById("nota3").value);
+
+  let media = (av1 + av2 + av3)/3 ;
+
+   if(media >= 6)
+     alert("O aluno está aprovado " + media);
+   else if (media <= 5)
+     alert("O aluno está reprovado" + media);
+   else
+     alert("O aluno está de recuperação" + media);
+ }
+
+
+ 
+
+
+ function comprar(){
+   
+  let p1 = parseFloat(document.getElementById("produto1").value);
+
+  let p2 = parseFloat(document.getElementById("produto2").value);
+
+  let dc = 5 ;
+  
+   let valor = p1 + p2;
+
+   let desconto = valor - dc;
+
+   alerte("O valor total da sua compra é de" + valor + "você ganhou desconto de  " + dc + "reais")
+   
+ }
---
+++ farmacia.js
@@ -0,0 +1,16 @@
+
+function farmacia(){
+  let fralda = 25
+  let leite  = 15
+  let pomada = 15
+  let lenço  = 10/2
+  let compras = fralda + leite + pomada ;
+  let  4item = compras + lenço ; 
+
+  if(compras){
+    alert( "total " + compras + " na  compra do lenço tem 50% de desconto ")
+
+  }else{
+   alert( "total " + 4item + "o 4 item saio com desconto")
+  }
+}
